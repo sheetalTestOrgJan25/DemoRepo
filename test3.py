d0f46b66df63e405d4094a1875a671a447c9e664
sheetal-bijale <sheetalbijale110@gmail.com>
2025-02-04 00:10:16 +0530

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,10 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
+print("Try programiz.pro")
+print("Try programiz.pro")
+print("Try programiz.pro")
+print("Try programiz.pro")
+print("Try programiz.pro")
+
+print("Try programiz.pro")
